405fc6d2a8cc695832bf94dcb66e041678e36014
Ranch <kevin.chen.16@stonybrook.edu>
2022-06-14 17:25:50 -0400

    Added textfields to modify charbuild

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import CharacterBuild from "./views/CharacterBuild";
 import GridDisplay from "./views/GridDisplay";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#90caf9"
+    },
+  },
+  typography: {
+    fontFamily: "genshinFont"
+  }
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CharacterBuild />
       <GridDisplay />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/views/CharacterBuild.js
+++ src/views/CharacterBuild.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { get, map, keys } from "lodash";
-import { Box } from "@material-ui/core";
+import { get, map, keys, omit } from "lodash";
+import { Box, TextField, makeStyles } from "@material-ui/core";
 import CharacterClass from "../classes/CharacterClass";
 import CharacterBaseDamage from "../utils/CharacterBaseDamage";
 
+const useStyles = makeStyles({
+  field: {
+    "&:hover .MuiInputLabel-root": {
+      color: "#90caf9", //Change hover color of text
+    },
+    "& .Mui-focused": {
+      color: "#90caf9", //Color of label when focused
+    }
+  }
+});
+
 export default function CharacterBuild() {
+  const classes = useStyles();
   // a character build that a user will give us
   const build = {
     character_name: "Chongyun",
@@ -20,14 +32,49 @@ export default function CharacterBuild() {
 
   return (
     <>
-      {map(keys(charBuild), (stats, idx) => {
+      <Box style={{ display: "flex", alignItems: "center" }}>
+        <Box
+          style={{
+            justifyContent: "flex-start",
+            minWidth: "250px",
+            textAlign: "right"
+          }}
+          marginRight="20px"
+        >
+          character_name
+        </Box>
+        <Box style={{ justifyContent: "flex-end", minWidth: "100px" }}>
+          {get(charBuild, "character_name")}
+        </Box>
+      </Box>
+      {map(keys(omit(charBuild, "character_name")), (stats, idx) => {
         return (
-          <Box key={idx} style={{ display: "flex" }}>
-            <Box style={{ justifyContent: "flex-start", minWidth: "250px" }}>
+          <Box key={idx} style={{ display: "flex", alignItems: "center" }}>
+            <Box
+              style={{
+                justifyContent: "flex-start",
+                minWidth: "250px",
+                textAlign: "right"
+              }}
+              marginRight="20px"
+            >
               {stats}
             </Box>
             <Box style={{ justifyContent: "flex-end", minWidth: "100px" }}>
-              {get(charBuild, stats)}
+              <TextField
+                type="number"
+                label={stats}
+                inputProps={{
+                  style: {
+                    color: "white",
+                    fontFamily: "genshinFont"
+                  }
+                }}
+                size="small"
+                className={classes.field}
+                onChange={e => (charBuild[stats] = parseFloat(e.target.value))}
+                defaultValue={get(charBuild, stats)}
+              ></TextField>
             </Box>
           </Box>
         );
