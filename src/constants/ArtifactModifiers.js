7062e0c9adda56af5c8d1cf12b1d23e409ec113b
Ranch <kevin.chen.16@stonybrook.edu>
2022-06-21 14:27:52 -0400

    added artifact info file

---
+++ src/constants/ArtifactModifiers.js
@@ -0,0 +1,30 @@
+const ArtifactModifiers = {
+  "4pc_noblesse": {
+    label: "4pc Noblesse",
+    field: "4pc_noblesse",
+    burst: 1.2,
+    atk: 1.2,
+  },
+  "4pc_emblem":{
+    label: "4pc Emblem",
+    field: "4pc_emblem",
+  },
+  "4pc_vv": {
+    label: "4pc Viridescent",
+    field: "4pc_vv",
+  },
+  "4pc_cw": {
+    label: "4pc Crimson Witch",
+    field: "4pc_cw",
+  },
+  "4pc_lw": {
+    label: "4pc Lavawalker",
+    field: "4pc_lw",
+  },
+  "4pc_instruc": {
+    label: "4pc Instructor",
+    field: "4pc_instruc",
+  },
+};
+
+export default ArtifactModifiers;
